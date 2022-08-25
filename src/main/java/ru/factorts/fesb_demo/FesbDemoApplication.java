52d1b68bd0e994c08ff85619280e4d5bc3dcbdf1
michael <lukyanovmiv@gmail.com>
2022-08-25 20:23:10 +0300

    Initial commit

---
+++ src/main/java/ru/factorts/fesb_demo/FesbDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.factorts.fesb_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FesbDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FesbDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/factorts/fesb_demo/FesbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.factorts.fesb_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FesbDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
